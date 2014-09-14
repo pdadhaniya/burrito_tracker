c56fe50cf9ae3f6389b9bc93429a616fb5161b6d
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-14 00:05:16 -0700

    add test to check if restaurant initializes

---
+++ spec/entities/restaurant_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../spec_helper.rb'
+
+describe BurritoTracker::Restaurant do
+  let(:restaurant1) { BurritoTracker::Restaurant.new(name: "Papalote") }
+
+  describe "#initialize" do
+    xit "should make a new restaurant" do
+      expect(restaurant1).to be_a(BurritoTracker::Restaurant)
+      expect(restaurant1.name).to eq("Papalote")
+    end
+  end
+
+
+
+end
