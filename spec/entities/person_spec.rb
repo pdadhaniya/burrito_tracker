95f78deecdef5ffb9f90ae32388440e01148e84d
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-13 23:56:06 -0700

    update test to have first_name attribute for person class

--- spec/entities/person_spec.rb
+++ spec/entities/person_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../spec_helper.rb'
 
 describe BurritoTracker::Person do
-  let(:person1) { BurritoTracker::Person.new(name: "Parag") }
+  let(:person1) { BurritoTracker::Person.new(first_name: "Parag") }
 
 
   describe "#initialize" do
     it "should make a new person with a name" do
       expect(person1).to be_a(BurritoTracker::Person)
-      expect(person1.name).to eq("Parag")
+      expect(person1.first_name).to eq("Parag")
     end
   end
 
