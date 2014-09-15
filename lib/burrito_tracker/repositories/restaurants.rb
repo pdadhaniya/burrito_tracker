2fe03075d0cb83d50df2c17636ee67f35221c679
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-15 08:07:54 -0700

    add methods to restaurants repo that are similar to burritos repo

--- lib/burrito_tracker/repositories/restaurants.rb
+++ lib/burrito_tracker/repositories/restaurants.rb
@@ -8,6 +8,47 @@ module BurritoTracker
         @db = PG.connect(host: 'localhost', dbname: 'burrito-tracker-db')
       end
 
+      def save_restaurant(restaurant)
+        if restaurant.id.nil?
+          command = <<-SQL
+          INSERT INTO restaurant ( name )
+          VALUES ( '#{restaurant.name}' )
+          RETURNING *;
+          SQL
+          result = @db.exec(command).first
+          restaurant.instance_variable_set("@id", (result["id"].to_i))
+          result
+        else
+          command = <<-SQL
+          UPDATE restaurants SET ( name ) = ( '#{restaurant.type}' )
+          WHERE id='#{restaurant.id}'
+          RETURNING *;
+          SQL
+          result = @db.exec(command).first
+        end
+      end
+
+      def delete_restaurant(restaurant)
+        command = <<-SQL
+        DELETE FROM restaurants WHERE id='#{restaurant.id}';
+        SQL
+        result = @db.exec(command)
+      end
+
+      def get_restaurant(id)
+        command = <<-SQL
+        SELECT * FROM restaurants WHERE id='#{id}';
+        SQL
+        result = @db.exec(command).first
+      end
+
+      def get_all_restaurants
+        command = <<-SQL
+        SELECT * FROM restaurants;
+        SQL
+        result = @db.exec(command).entries
+      end
+
       def create_tables
         command = <<-SQL
         CREATE TABLE IF NOT EXISTS restaurants (id SERIAL PRIMARY KEY, name TEXT);
